c5bbcee849957002b49fb115af1865cb9d6cc217
sathish-strategyfox <--global>
2025-08-05 15:47:18 +0530

    feat(environment): add SovereignAtrium environment with model, lighting, and placeholder data
    - Introduced SovereignAtrium environment data, including player settings and placeholder data.
    - Created SovereignAtrium model and lighting components for rendering in the environment.
    - Updated Ground and Lights components to support SovereignAtrium, enhancing the overall environment experience.

--- src/data/environment/EnvironmentData.ts
+++ src/data/environment/EnvironmentData.ts
@@ -19,6 +19,7 @@ import PetalPavilionPlaceHolderData from "./placeHolderData/PetalPavilion";
 import AutoHavenPlaceHolderData from "./placeHolderData/AutoHaven";
 import BarbieClutchPlaceHolderData from "./placeHolderData/BarbieClutch";
 import MerryMeadowPlaceHolderData from "./placeHolderData/MerryMeadow";
+import SovereignAtriumPlaceHolderData from "./placeHolderData/SovereignAtrium";
 
 interface EnvironmentData {
   [environment_name: string]: {
@@ -687,7 +688,8 @@ const environmentData: EnvironmentData = {
         scale: 4,
       },
     ],
-  },MERRYMEADOW: {
+  },
+  MERRYMEADOW: {
     playerSpeed: 17,
     playerHeight: 4,
     placeHolderData: MerryMeadowPlaceHolderData,
@@ -717,6 +719,39 @@ const environmentData: EnvironmentData = {
         position: [23.150, 2.766, -1.351],
         rotation: [0, -90 - 45, 0],
         scale: 3,
+      },
+    ],
+  },
+  SOVEREIGNATRIUM: {
+    playerSpeed: 17,
+    playerHeight: 4,
+    placeHolderData: SovereignAtriumPlaceHolderData,
+    initialGSAP: {
+      start: {
+        position: [0, 6, 0],
+        rotation: [5, 30, 0],
+        duration: 0,
+      },
+      update: [
+        {
+          position: [0, 6, 15],
+          rotation: [5, 0, 0],
+          duration: 2,
+        },
+      ],
+    },
+    televisions: [
+      {
+        position: [0, 5.400, 18.5],
+        rotation: [0, 90, 0],
+        scale: 0.45,
+      },
+    ],
+    brandPosters: [
+      {
+        position: [-12, 5.400, -18.7],
+        rotation: [0, 0 , 0],
+        scale: 2,
       },
     ],
   },
